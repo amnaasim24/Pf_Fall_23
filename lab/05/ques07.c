ed9f5a17b35b4c1ce3cd4dc223a12cad853cce1c
amnaasim24 <142867835+amnaasim24@users.noreply.github.com>
2023-09-21 02:30:54 +0500

    Update ques07.c

--- lab/05/ques07.c
+++ lab/05/ques07.c
@@ -1,6 +1,6 @@
 //Programmer:Amna Asim
 //Lab Task 05
-//Question 7
+//Question 07
 
 #include<stdio.h>
 void main()  {
@@ -9,17 +9,14 @@ void main()  {
 	
 	printf("Please enter the cost:\t");
 	scanf("%lf",&cost);
-	if(cost>1999&&cost<2000){
-		discount=0.5;
+	if(cost>1999&&cost<=4000){
+		discount=0.2;
 	}
-		else if(cost>6000) {
-			discount=0.5;
-		}
-		else if(cost>=2000&&cost<=4000){
-			discount=0.2;
-		}
 		else if(cost>=4001&&cost<=6000){
 			discount=0.3;
+		}
+		else if(cost>6000) {
+			discount=0.5;
 		}
 	else {
 		discount=0;
